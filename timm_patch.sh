2f79f4cba9cad60d4e40ce1403df582df6c4d580
siv <subhashpadala18@gmail.com>
2024-04-05 17:13:59 +0530

    basic setup on my system

--- timm_patch.sh
+++ timm_patch.sh
@@ -1,3 +1,3 @@
-timm_path=/private/home/berniehuang/.local/lib/python3.8/site-packages/timm/models/
+timm_path=/Users/subhashp/Documents/Open-Source/transformers/.env/lib/python3.10/site-packages/timm/models/
 cp timm_patch/swin_transformer.py $timm_path
 cp timm_patch/helpers.py $timm_path/layers/

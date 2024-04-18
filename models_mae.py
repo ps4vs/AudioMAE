c1d62f1eb2132d68aab3b5cbc1573a2086ad348b
siv <subhashpadala18@gmail.com>
2024-04-18 14:40:17 +0530

    updated swin-transformer block

--- models_mae.py
+++ models_mae.py
@@ -20,7 +20,7 @@ from timm.models.vision_transformer import Block
 from util.pos_embed import get_2d_sincos_pos_embed, get_2d_sincos_pos_embed_flexible, get_1d_sincos_pos_embed_from_grid
 from util.misc import concat_all_gather
 from util.patch_embed import PatchEmbed_new, PatchEmbed_org
-from timm.models.swin_transformer import SwinTransformerBlock
+from timm.models.swin_transformer_v2_cr import SwinTransformerV2CrBlock
 
 class MaskedAutoencoderViT(nn.Module):
     """ Masked Autoencoder with VisionTransformer backbone
